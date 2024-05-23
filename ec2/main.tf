6bfe962c7e19eaea317634e7bba08353d5e51980
ejwoods <ewoods08@gmail.com>
2024-05-23 18:43:22 -0400

    create ec2/main.tf
    uses modules defined in vpc.tf, security.tf, and instance.tf

---
+++ ec2/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vcp" {
+  source = "./vpc"
+}
+
+module "security" {
+  source = "./security"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "ec2" {
+  source = "./instance"
+  vpc_id = module.vpc.vpc_id
+  subnet_id = module.vpc.public_subnet_id
+  security_group_id = module.security.ecs_sg_id
+}
