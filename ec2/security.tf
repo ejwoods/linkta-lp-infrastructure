15239c60b8a0e478b3b7c8f456b26d9f671ec524
ejwoods <ewoods08@gmail.com>
2024-05-23 18:06:08 -0400

    update in/egress rules in security.tf
    - removed in-line rules for ingress and egress in ecs_sg security group to follow terraform docs recommendation
    - added aws_vpc_security_group_ingress_rule and aws_vpc_security_group_egress_rule resources to handle sg rules instead
    - added commented-out resources for future HTTPS and DNS setup

--- ec2/security.tf
+++ ec2/security.tf
@@ -1,21 +1,45 @@
 resource "aws_security_group" "ecs_sg" {
   vpc_id = aws_vpc.main.id
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   tags = {
     Name = "ecs-sg"
   }
 }
+
+# TODO: Remove HTTP rules when HTTPS is fully implemented
+resource "aws_vpc_security_group_ingress_rule" "http_ingress" {
+  security_group_id = aws_security_group.ecs_sg.id
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# TODO: Remove HTTP rules when HTTPS is fully implemented
+resource "aws_vpc_security_group_egress_rule" "http_egress" {
+  security_group_id = aws_security_group.ecs_sg.id
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# Future setup for HTTPS traffic
+# resource "aws_vpc_security_group_egress_rule" "https_egress" {
+#   security_group_id = aws_security_group.ecs_sg.id
+#   from_port         = 443
+#   to_port           = 443
+#   protocol          = "tcp"
+#   cidr_blocks       = ["0.0.0.0/0"]
+# }
+
+# Future setup for DNS traffic
+# resource "aws_vpc_security_group_egress_rule" "dns_egress" {
+#   security_group_id = aws_security_group.ecs_sg.id
+#   from_port         = 53
+#   to_port           = 53
+#   protocol          = "udp"
+#   cidr_blocks       = ["0.0.0.0/0"]
+# }
 
 output "ecs_sg_id" {
   value = aws_security_group.ecs_sg.id
