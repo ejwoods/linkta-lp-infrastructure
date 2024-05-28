e484363f1c33a5c997ee5bb8bddfe9817cfe9339
ejwoods <ewoods08@gmail.com>
2024-05-28 09:02:36 -0400

    update vpc state use in ec2/main.tf
    update ec2/main.tf to use data resource for vpc instead of module resource. This allows ec2 module to access vpc module's state file and avoid re-creation of resources.

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,18 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "vpc" {
-  source = "../vpc"
+data "terraform_remote_state" "vpc" {
+  backend = "local"
+
+  config = {
+    path = "../vpc/terraform.tfstate"
+  }
 }
 
 module "security" {
   source = "./security"
-  vpc_id = module.vpc.vpc_id
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 }
 
 module "ec2" {
   source = "./instance"
-  vpc_id = module.vpc.vpc_id
-  public_subnet_id = module.vpc.public_subnet_id
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
+  public_subnet_id = data.terraform_remote_state.vpc.outputs.public_subnet_id
   ecs_sg_id = module.security.ecs_sg_id
 }
